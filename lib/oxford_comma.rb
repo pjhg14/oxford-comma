787f53836ea295661fd47e0aa05466d7c0d06472
pj <pjhg14@gmail.com>
2021-04-25 17:25:04 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+    last_element = array[-1]
 
+    if array.size == 1
+        return array.join
+    end
+
+    array[-1] = "and #{last_element}"
+
+    if array.size == 2
+        return array.join(" ")
+    end
+
+    array.join(", ")
 end
